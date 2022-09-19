db44db16f0b976df7077bc9e1f0adefd90f005ba
Beth Massi <bethma@microsoft.com>
2022-09-19 14:52:38 -0700

    Add multi-window

--- src/MAUI-PointOfSale/PointOfSale/Platforms/MacCatalyst/SceneDelegate.cs
+++ src/MAUI-PointOfSale/PointOfSale/Platforms/MacCatalyst/SceneDelegate.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Foundation;
+using Microsoft.Maui;
+using UIKit;
 
-namespace PointOfSale.Platforms.MacCatalyst
+namespace PointOfSale;
+
+[Register("SceneDelegate")]
+public class SceneDelegate : MauiUISceneDelegate
 {
-    class SceneDelegate
-    {
-    }
 }
