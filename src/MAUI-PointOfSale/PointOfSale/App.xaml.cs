f9ec6848caeb309b0e508393019990e8b99e4fd1
Beth Massi <bethma@microsoft.com>
2022-09-23 15:25:10 -0700

    Add simple AppActions example

--- src/MAUI-PointOfSale/PointOfSale/App.xaml.cs
+++ src/MAUI-PointOfSale/PointOfSale/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace PointOfSale;
+﻿using System.Linq.Expressions;
+
+namespace PointOfSale;
 
 public partial class App : Application
 {
@@ -7,7 +9,7 @@ public partial class App : Application
 		InitializeComponent();
 
         App.Current.UserAppTheme = AppTheme.Dark;
-
+            
         if (DeviceInfo.Idiom == DeviceIdiom.Phone)
         {
             MainPage = new AppShellMobile();
@@ -16,5 +18,23 @@ public partial class App : Application
         {
             MainPage = new AppShell();
         }
-	}
+
+
+        AppActions.SetAsync(
+           new AppAction("add_product", "Add Product"),
+           new AppAction("view_menu", "View Menu")
+           );
+        AppActions.OnAppAction += AppActions_OnAppAction;
+    }
+
+    private void AppActions_OnAppAction(object sender, AppActionEventArgs e)
+    {
+        if (e.AppAction.Id == "add_product")
+        {
+            //do something
+        }
+        else if (e.AppAction.Id=="view_menu")
+        {
+        }
+    }
 }
